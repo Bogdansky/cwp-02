55c8883f41f34a198a8929f1486ce108ac29cc86
Bogdan <bogdya2@gmail.com>
2018-09-25 21:45:24 +0300

    Talking success

--- client.js
+++ client.js
@@ -1,20 +1,51 @@
 const net = require('net');
+const fs = require('fs');
 const port = 8124;
 
 const client = new net.Socket();
+
+let questions = {};
+let answers = "";
 
 client.setEncoding('utf8');
 
 client.connect(port, function() {
     console.log('Connected');
-    client.write('\r\nHello, Server!\r\nLove,\r\nClient.\r\n');
+    questions = JSON.parse(fs.readFileSync('qa.json'));
+    questions.sort( (a,b) => {
+        return Math.random() - 0.5;
+    });
+    client.write('QA');
 });
 
-client.on('data', function(data) {
-    console.log(data);
-    client.destroy();
+let enumerator = 0;
+client.on('data',(data) => {
+    if (data==='DEC'){
+        client.destroy();
+    }
+    else{
+        let message = "";
+        console.log(enumerator);
+        if(enumerator===questions.length){
+            console.log(...answers);
+            client.destroy();
+        }
+        else {
+            if (enumerator>0){
+                message = data===questions[enumerator-1].answer ? "Right\n" : "Wrong\n";
+                answers+=questions[enumerator-1].question+": "+message;
+            }
+            client.write(questions[enumerator].question);
+            enumerator++;
+        }
+    }
 });
 
-client.on('close', function() {
+client.on('close',() => {
     console.log('Connection closed');
+});
+
+client.on('error', () =>{
+    client.destroy();
+    console.log("Unexpected error");
 });
--- server.js
+++ server.js
@@ -1,20 +1,55 @@
 const net = require('net');
+const fs = require('fs');
 const port = 8124;
+
 let sid = 0;
 let identifier;
 
+let opened = false;
+
 const server = net.createServer((client) => {
+
     identifier = Date.now()+ ++sid;
+    let streamer = fs.createWriteStream(`logs\\${identifier}.log`);
     console.log(`Client ${identifier} connected`);
 
     client.setEncoding('utf8');
 
     client.on('data', (data) => {
         console.log(data);
-        client.write('\r\nHello!\r\nRegards,\r\nServer\r\n');
+        streamer.write(data+"\n");
+        if (!opened){
+            if (data === 'QA') {
+                opened = true;
+                streamer.write("Server: ACK\n");
+                client.write("ACK");
+            }
+            else {
+                streamer.write("Server: DEC\n");
+                client.write("DEC");
+                client.end();
+            }
+        }
+        else{
+            let answer = ['Yes','No'][Math.random() < 0.5 ? 0 : 1];
+            client.write(answer);
+            console.log(answer);
+        }
     });
 
-    client.on('end', () => console.log('Client disconnected'));
+    client.on('end', () => {
+        console.log('Client disconnected');
+        streamer.write(`Client ${identifier} disconnected\n`);
+        streamer.end();
+        opened = false;
+    });
+
+    client.on('error', (error) => {
+        console.log(error.message);
+        streamer.end();
+        server.close();
+    });
+
 });
 
 server.listen(port, () => {
